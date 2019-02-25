b375e22a2767eb76067f9b9a80ec411ac654046d
microsong <jhn06046@hotmail.com>
2019-02-25 13:48:53 +0900

    enum 수정

--- CSharp_Self_Study/day12_복사, ref타입, enum/enum 응용.cs
+++ CSharp_Self_Study/day12_복사, ref타입, enum/enum 응용.cs
@@ -6,22 +6,23 @@ using System.Threading.Tasks;
 
 namespace CShaap_Self_Study.day12_복사__ref타입__enum
 {
-    public static int Calc(char onType, int oprend1, int oprend2)
-    {
-        switch (onType)
-        {
-            case '+': return oprend1 + oprend2;
-            case '-': return oprend1 - oprend2;
-            case '*': return oprend1 * oprend2;
-            case '/': return oprend1 / oprend2;
-        }
-        return 0;
-    }
+    enum CalcType { Add, Minus, Multiply, Divide };
     class enum_응용
     {
+        public static int Calc(CalcType onType, int oprend1, int oprend2)
+        {
+            switch (onType)
+            {
+                case CalcType.Add : return oprend1 + oprend2;
+                case CalcType.Minus: return oprend1 - oprend2;
+                case CalcType.Multiply: return oprend1 * oprend2;
+                case CalcType.Divide: return oprend1 / oprend2;
+            }
+            return 0;
+        }
         static void Main(string[] args)
         {
-
+            Console.WriteLine(Calc(CalcType.Add, 5, 6));    //enum 인스턴스 메서드로 전달
         }
     }
 }
