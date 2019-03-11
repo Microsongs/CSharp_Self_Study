da81e4c1e3cc449bade3183cbcd9a810cbfc6bf5
microsong <jhn06046@hotmail.com>
2019-03-11 09:49:19 +0900

    Day19 플랫폼 선택과 버전 관리

---
+++ CSharp_Self_Study/day19_플랫폼 선택과 버전 관리/Public_Key_Infrastructure.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Reflection;
+
+//어셈블리 버전 특성
+//Properties의 AssemblyInfo에 정보가 들어가있음
+//[assembly : AssemblyVersion("1.0.0.1")]
+
+namespace CSharp_Self_Study.day19_플랫폼_선택
+{
+    class Public_Key_Infrastructure
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
