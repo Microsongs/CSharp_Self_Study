dbc24610b1ef9188ac558de77032bbbfb6e13a3b
microsong <jhn06046@hotmail.com>
2019-03-12 14:35:53 +0900

    Day20 - 예외와 어셈블리(예외처리 X)

---
+++ CSharp_Self_Study/day20_Exception/ExceptionCode.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharp_Self_Study.day20_Exception
+{
+    class ExceptionCode
+    {
+        static void Main(string[] args)
+        {
+                string txt = null;
+                string upper = txt.ToUpper();
+        }
+    }
+}
