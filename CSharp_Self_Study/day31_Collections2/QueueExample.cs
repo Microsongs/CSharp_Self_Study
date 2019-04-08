9fbed0a189f4ab8058f872694d6ae5fb965b6d63
microsong <jhn06046@hotmail.com>
2019-04-08 10:39:23 +0900

    day31_Collections2

---
+++ CSharp_Self_Study/day31_Collections2/QueueExample.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharp_Self_Study.day31_Collections2
+{
+    class QueueExample
+    {
+        static void Main(string[] args)
+        {
+            Queue q = new Queue();
+
+            q.Enqueue(1);
+            q.Enqueue(5);
+            q.Enqueue(3);
+
+            int First = (int)q.Dequeue();
+
+            q.Enqueue(7);
+
+            while (q.Count > 0)
+                Console.Write(q.Dequeue() + ", ");
+            //큐를 Dequeue 없이 비우고 싶다면 q.Clear() 메서드 호출
+        }
+    }
+}
---
+++ CSharp_Self_Study/day31_Collections2/StackExample.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharp_Self_Study.day31_Collections2
+{
+    class StackExample
+    {
+        static void Main(string[] args)
+        {
+            Stack st = new Stack();
+
+            st.Push(1);
+            st.Push(5);
+            st.Push(3);
+
+            int Last = (int)st.Pop();
+            st.Push(7);
+
+            while (st.Count > 0)
+                Console.Write(st.Pop() + ", ");
+            //stack을 Pop없이 비울 경우 st.Clear 메서드 사용
+        }
+    }
+}
